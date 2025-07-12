16af486a32d44f43ee8c21fc5c9c9d7609b90bd4
Daniyar <Daniyar@DESKTOP-2N3V8QS>
2025-07-13 01:41:52 +0600

    coding filter block with js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,50 @@
+  
   const header = document.querySelector('.header');
+  const categoriesAsideItems = document.querySelectorAll('.categories-aside__item');
+  //const dropDown = document.querySelector(".categories-aside__dropdown");
+
+
+
+//  function openCategoryList(){
+
+  categoriesAsideItems.forEach(item => {
+    item.addEventListener('click', (event) => {
+      // Code to execute when an <li> is clicked
+      const dropDown = item.querySelector(".categories-aside__dropdown");
+
+
+      console.log(item)
+   
+     item.classList.toggle("is-open");
+     // item.style.backgroundColor = 'gray'; // Example: change background color
+     console.log(dropDown.style.display);
+     
+    // dropDown.style.display = "none" ? 'flex': 'none';
+
+     dropDown.classList.toggle("to-show");
+    
+
+     
+
+
+      });
+
+    });
+//  }
+
+
+
+  // function fix() {
+  //      if(dropDown.style.display ==="none") {
+  //       dropDown.style.display = "flex";
+  //    }else{
+  //        dropDown.style.display = "none";
+  //    }
+  
+  // }
+
+
+
 
   function updateHeaderClass() {
     if (window.scrollY > 80) {
@@ -12,6 +58,8 @@
 
   // Проверка при загрузке страницы
   window.addEventListener('load', updateHeaderClass);
+ 
 
   // Проверка при скролле
-  window.addEventListener('scroll', updateHeaderClass);
+ // window.addEventListener('scroll', updateHeaderClass);
+

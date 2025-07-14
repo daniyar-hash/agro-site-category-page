7e678f12782349494fa32acd4ca08c4f8cf02b18
Daniyar <Daniyar@DESKTOP-2N3V8QS>
2025-07-14 18:13:20 +0600

    finishing function js for sort-button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,111 @@
   
   const header = document.querySelector('.header');
-  const categoriesAsideItems = document.querySelectorAll('.categories-aside__item');
-  //const dropDown = document.querySelector(".categories-aside__dropdown");
+  const categoriesAsideItems  = document.querySelectorAll('.categories-aside__item');
+  const sorterVeiewButtons    = document.querySelectorAll(".category-products_sort-button");
+  const categoryProductslist  = document.querySelector(".category-products__list");
+  const categoryProductsItems = document.querySelectorAll(".category-products__item");
 
 
+
+  sorterVeiewButtons.forEach(item =>{
+    item.addEventListener("click", (event)=>{
+
+
+      if(item.classList.contains("list")){
+          categoryProductslist.classList.add("flex-row");
+          categoryProductslist.classList.remove("grid", "grid--3");
+          addClassProductsItem(categoryProductsItems, "item-row");
+          wrapInnerElement({itemSelector:'.category-products__item',
+             targetSelector1:'.category-products__title',targetSelector2:'.category-products__price',
+              wrapperClass:'category-products__wrapper'});
+
+          wrapInnerElement({itemSelector:'.category-products__item', 
+            targetSelector1:'.category-products__button', wrapperClass:'category-products__button-wrapper'});
+
+
+
+      } else{
+          categoryProductslist.classList.remove("flex-row");
+          categoryProductslist.classList.add("grid", "grid--3");
+          removeClassProductsItem(categoryProductsItems, "item-row");
+          deleteWrapInnerElement('.category-products__wrapper');
+          deleteWrapInnerElement('.category-products__button-wrapper');
+
+
+      }
+
+
+
+    })
+  })
+
+
+
+
+
+function wrapInnerElement({ itemSelector, targetSelector1,targetSelector2=null, wrapperClass }) {
+  document.querySelectorAll(itemSelector).forEach(item => {
+    const target1 = item.querySelector(targetSelector1);
+    const target2 = item.querySelector(targetSelector2);
+
+
+
+    if (!target1) return;
+
+
+    const wrapper = document.createElement('div');
+    wrapper.classList.add(wrapperClass);
+
+    target1.parentNode.insertBefore(wrapper, target1);
+    wrapper.appendChild(target1);
+
+    if (!target2) return;
+
+    if(target2){
+        wrapper.appendChild(target2);
+    }
+
+
+
+
+
+
+  });
+}
+
+function deleteWrapInnerElement(wrapperClass) {
+  document.querySelectorAll(wrapperClass).forEach(wrapper => {
+
+    const parent = wrapper.parentNode;
+
+    while(wrapper.firstChild){
+      parent.insertBefore(wrapper.firstChild,wrapper)
+    }
+
+    parent.removeChild(wrapper);
+
+
+  });
+}
+
+
+
+
+
+
+    function addClassProductsItem(elementList, className){
+      elementList.forEach(item=>{
+     
+        item.classList.add(`${className}`)
+      })
+    }
+
+    
+    function removeClassProductsItem(elementList, className){
+      elementList.forEach(item=>{
+      item.classList.remove(`${className}`)
+      })
+    }
 
 //  function openCategoryList(){
 
@@ -13,11 +115,11 @@
       const dropDown = item.querySelector(".categories-aside__dropdown");
 
 
-      console.log(item)
+
    
      item.classList.toggle("is-open");
      // item.style.backgroundColor = 'gray'; // Example: change background color
-     console.log(dropDown.style.display);
+    
      
     // dropDown.style.display = "none" ? 'flex': 'none';
 
@@ -33,19 +135,6 @@
 //  }
 
 
-
-  // function fix() {
-  //      if(dropDown.style.display ==="none") {
-  //       dropDown.style.display = "flex";
-  //    }else{
-  //        dropDown.style.display = "none";
-  //    }
-  
-  // }
-
-
-
-
   function updateHeaderClass() {
     if (window.scrollY > 80) {
       header.classList.remove('transparent');
@@ -59,7 +148,9 @@
   // Проверка при загрузке страницы
   window.addEventListener('load', updateHeaderClass);
  
-
   // Проверка при скролле
- // window.addEventListener('scroll', updateHeaderClass);
+  window.addEventListener('scroll', updateHeaderClass);
+
+
+ 
 
